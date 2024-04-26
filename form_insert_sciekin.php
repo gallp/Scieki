63cdfa5a9feb3a8504594314b6e78c7ced34315d
P.Galicki <gallp1@wp.pl>
2024-04-26 15:08:21 +0200

    new css

--- form_insert_sciekin.php
+++ form_insert_sciekin.php
@@ -7,8 +7,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
-
-// Include config file
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,7 +18,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
 
 <b> Witaj, <?php echo htmlspecialchars($_SESSION["username"]);?> </b>
-<header>
+<header class="header-main">
     <section class="first-header-section">
     </section>
     
@@ -37,32 +35,66 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </section>
 </header>
 
-    <h1>Scieki nieoczyszczone/..nowy </h1>
-    
+<section class="body-middle">
+     
+    <section class="container">
+        <header>Scieki nieoczyszczone\Nowy wpis</header>
         <form class="form-basic" action="model_insert_sciekin.php" method="POST">
-            <label for="Chlorki">Chlorki:</label>
-            <input type="text" id="Chlorki" name="Chlorki" pattern="(\d{0,3})(\.[\d]{1,3})?">
-            <label for="Chrom">Chrom:</label>
-            <input type="text" id="Chrom" name="Chrom" pattern="(\d{0,3})(\.[\d]{1,3})?">
-            <label for="Cynk">Cynk:</label>
-            <input type="text" id="Cynk" name="Cynk" pattern="(\d{0,3})(\.[\d]{1,3})?">
-            <label for="Kadm">Kadm:</label>
-            <input type="text" id="Kadm" name="Kadm" pattern="(\d{0,3})(\.[\d]{1,3})?">
-            <label for="Miedz">Miedź:</label>
-            <input type="text" id="Miedz" name="Miedz" pattern="(\d{0,3})(\.[\d]{1,3})?">
-            <label for="Nikiel">Nikiel:</label>
-            <input type="text" id="Nikiel" name="Nikiel" pattern="(\d{0,3})(\.[\d]{1,3})?">
-            <label for="Odczyn">Odczyn:</label>
-            <input type="text" id="Odczyn" name="Odczyn" pattern="(\d{0,3})(\.[\d]{1,3})?">
-            <label for="Olow">Olów:</label>
-            <input type="text" id="Olow" name="Olow" pattern="(\d{0,3})(\.[\d]{1,3})?">
-            <label for="Siarczany">Siarczany:</label>
-            <input type="text" id="Siarczany" name="Siarczany" pattern="(\d{0,3})(\.[\d]{1,3})?"><br>         
-            <section>
-            <input type="submit" class="btn btn-send" value="Zapisz">
-            <a href="sciekin.php" class="btn btn-error">Anuluj</a>
-        </section>
+            <div class="input-box-group">
+                <div class="input-box">
+                    <label for="Chlorki">Chlorki:</label>
+                    <input type="text" id="Chlorki" name="Chlorki" pattern="(\d{0,3})(\.[\d]{1,3})?">
+                </div>
+                <div class="input-box">
+                    <label for="Chrom">Chrom:</label>
+                    <input type="text" id="Chrom" name="Chrom" pattern="(\d{0,3})(\.[\d]{1,3})?">
+                </div>
+                <div class="input-box">
+                    <label for="Cynk">Cynk:</label>
+                    <input type="text" id="Cynk" name="Cynk" pattern="(\d{0,3})(\.[\d]{1,3})?">
+                </div>
+            </div>
+            <div class="input-box-group">
+                <div class="input-box">
+                    <label for="Kadm">Kadm:</label>
+                    <input type="text" id="Kadm" name="Kadm" pattern="(\d{0,3})(\.[\d]{1,3})?">
+                </div>
+                <div class="input-box">
+                    <label for="Miedz">Miedź:</label>
+                    <input type="text" id="Miedz" name="Miedz" pattern="(\d{0,3})(\.[\d]{1,3})?">
+                </div>
+                <div class="input-box">
+                    <label for="Nikiel">Nikiel:</label>
+                    <input type="text" id="Nikiel" name="Nikiel" pattern="(\d{0,3})(\.[\d]{1,3})?">
+                </div>
+            </div>
+            <div class="input-box-group">
+                <div class="input-box">
+                    <label for="Odczyn">Odczyn:</label>
+                    <input type="text" id="Odczyn" name="Odczyn" pattern="(\d{0,3})(\.[\d]{1,3})?">
+                </div>
+                <div class="input-box">
+                    <label for="Olow">Ołów:</label>
+                    <input type="text" id="Olow" name="Olow" pattern="(\d{0,3})(\.[\d]{1,3})?">
+                </div>
+                <div class="input-box">
+                    <label for="Siarczany">Siarczany:</label>
+                    <input type="text" id="Siarczany" name="Siarczany" pattern="(\d{0,3})(\.[\d]{1,3})?"><br>
+                </div>
+            </div>
+            <div class="input-box-group">
+                <div class="input-box">
+                    <label for="Komentarz">Komentarz:</label>
+                    <textarea id="Komentarz" name="Komentarz"></textarea>
+                </div>
+            </div>
+            <div class="input-box-group">  
+                <div class="input-group">         
+                    <input type="submit" class="btn btn-send" value="Zapisz">
+                    <a href="sciekin.php" class="btn btn-error">Anuluj</a>
+                </div>
+            </div>
     </form>
-</div>
+ </div>
 </body>
 </html>
